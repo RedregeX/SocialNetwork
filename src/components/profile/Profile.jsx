c6582efccd4f5c3df1e945ca7d6c72ced46a35e5
seser <serega.lefortovo@gmail.com>
2022-04-01 16:27:11 +0300

    Added posts, changed size of pics

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -6,12 +6,17 @@ function Profile() {
         <div class = "profile">
             <div className="aboutme">
                 <img src={steve} alt="photo" class="steve"/>
-                <p>Steve</p>
-                <p>Jobs</p>
+                <p class="name">Steve Jobs</p>
             </div>
             <div className="posts">
+                <p class="myposts">My posts</p>
                 <input type="text" placeholder='Enter the post'/>
                 <button>Add a post</button>
+                <div className="post">
+                    <img src={steve} alt="miniphoto" class="ministeve"/>
+                    <span>Steve Jobs</span>
+                    <p>Here will be text</p>
+                </div>
             </div>
         </div>
     );
