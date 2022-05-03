235876da8473126c9c8d314410338a59903e2d4e
seser <serega.lefortovo@gmail.com>
2022-05-03 20:34:53 +0300

    New ways

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -4,20 +4,23 @@ import {Route} from 'react-router-dom';
 import DII from "./dialogIsItem/DII";
 import Messages from "./messages/Messages";
 
-
+let DialogNames = [{name:"Elon Musk", id:1}, {name:"Rick Astley", id:2}, {name:"Silvester Stallone", id:3}];
+let DialogMessages = [{message:"When is apple going to shutdown", id:1}, {message:"This is bla bla bla", id:2}, {message:"You are dead", id:3}]
 function Dialogs() {
     return (
         <div className="dialogs">
             <div className="usmes">
                 <div className="Usernames">
-                    <DII name={"Elon Musk"} id={"1"}/>
-                    <DII name={"Rick Astley"} id={"2"}/>
-                    <DII name={"Silvester Stallone"} id={"3"}/>
+                    {/* <DII name={DialogNames[0].name} id={DialogNames[0].id}/>
+                    <DII name={DialogNames[1].name} id={DialogNames[1].id}/>
+                    <DII name={DialogNames[2].name} id={DialogNames[2].id}/> */}
+                    {DialogNames.map(e => <DII  name={e.name} id={e.id}/>)}
                 </div> 
                 <div className="messages">
-                    <Messages message={"When is apple going to shutdown"} />
-                    <Messages message={"This is bla bla bla"} />
-                    <Messages message={"You are dead"} />
+                    {/* <Messages message={DialogMessages[0].message} id={DialogMessages[0].id} />
+                    <Messages message={DialogMessages[1].message} id={DialogMessages[1].id} />
+                    <Messages message={DialogMessages[2].message} id={DialogMessages[2].id} /> */}
+                    {DialogMessages.map(e => <Messages  message={e.message} id={e.id}/>)}
                 </div>
             </div>
             <div className="binput">
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,20 +1,11 @@
 import './Profile.css';
-import steve from'../../images/SteveJobs.jpg';
 import Posts from './posts/Posts';
+import ProfileInfo from './profileinfo/ProfileInfo';
 
 function Profile() {
     return (
         <div className = "profile">
-            <div className="aboutme">
-                <img src={steve} alt="photo" class="steve"/>
-                <div className="info">
-                    <p class="name">Steve Jobs</p>
-                    <div className="worknplace">
-                        <p className="from">Lives in California</p>
-                        <p className="job">Works at Apple</p>
-                    </div>
-                </div>
-            </div>
+            <ProfileInfo />
             <Posts />
         </div>
     );
